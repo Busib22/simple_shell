9f15bf13a186eae212cd17b2720b26b780cb5555
Busib22 <139709137+Busib22@users.noreply.github.com>
2023-10-18 16:57:42 -0700

    environment file

---
+++ sanit.c
@@ -0,0 +1,42 @@
+#include "main.h"
+
+/**
+ *_env - sanitational environmantal variable
+*/
+void _env(void)
+{
+	int k = 0;
+
+	while (environ[k])
+	{
+		printf("%s\n", environ[k]);
+		k++;
+	}
+}
+
+/**
+ * _vnget - its a big deal
+ * @env_var: variattin nii
+ * Return: table of value in code
+ */
+
+char *_vnget(char *env_var)
+{
+	int k = 0, zil;
+	int status;
+
+	while (environ[k])
+	{
+		status = 1;
+
+		for (zil = 0; environ[k][zil] != '='; zil++)
+		{
+			if (environ[k][zil] != env_var[zil])
+				status = 0;
+		}
+		if (status == 1)
+			break;
+		k++;
+	}
+	return (&environ[k][zil + 1]);
+}
