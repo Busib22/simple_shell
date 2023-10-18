47c59f3ec32fa2d1a2dc90b8df9cb499738e8b3d
Busib22 <139709137+Busib22@users.noreply.github.com>
2023-10-18 16:55:48 -0700

    header file

---
+++ main.h
@@ -0,0 +1,27 @@
+#ifndef SHELL_H
+#define SHELL_H
+
+#include <stdio.h>
+#include <sys/stat.h>
+#include <unistd.h>
+#include <sys/wait.h>
+#include <string.h>
+#include <stdlib.h>
+
+
+extern char **environ;
+
+int _strcmp(char *hamz1, char *hamz2);
+char *_strcpy(char *dest, char *src);
+char *awari_Path(char *jagjagun);
+char **_split(char *alli, char *sep);
+char *_strcat(char *dest, char *src);
+int _strlen(char *s);
+char *_vnget(char *env_var);
+void *_pelog(unsigned int berray, unsigned int gbangban);
+void _env(void);
+int killem(char **args);
+int nada_ln(char *lubbf_F);
+
+
+#endif
