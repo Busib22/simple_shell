51955cc1e63fd43cef69b70cac885f6ae2682e80
Busib22 <139709137+Busib22@users.noreply.github.com>
2023-10-18 16:56:29 -0700

    memory file

---
+++ medi_mem.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * _pelog - yoruba word for calllog
+ * @berray: betty array
+ * @gbangban: shoe ghangban
+ * Return: betty and kimber
+ */
+
+void *_pelog(unsigned int berray, unsigned int gbangban)
+{
+	unsigned int sunday = 0;
+	char *soldier = NULL;
+
+	if (berray == 0 || gbangban == 0)
+		return (NULL);
+
+	soldier = malloc(berray * gbangban);
+
+	if (soldier == NULL)
+		return (NULL);
+
+	for (; sunday < (berray * gbangban); sunday++)
+		soldier[sunday] = 0;
+
+	return (soldier);
+}
