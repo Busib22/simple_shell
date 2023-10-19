87db331515c1df40e7443a5cae244f389ff43dc4
Medinatu <114024115+Medinatu@users.noreply.github.com>
2023-10-18 17:14:11 -0700

    Update main.h

--- main.h
+++ main.h
@@ -1,9 +1,9 @@
 #ifndef SHELL_H
 #define SHELL_H
 
-include <stdio.h>
-include <sys/stat.h>
-include <unistd.h>
+#include <stdio.h>
+#include <sys/stat.h>
+#include <unistd.h>
 #include <sys/wait.h>
 #include <string.h>
 #include <stdlib.h>
